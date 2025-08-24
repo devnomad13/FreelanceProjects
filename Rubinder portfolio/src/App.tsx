0bdfb6dad0e33a11ac6c738ae052a646e5faaf4b
Dev Nomad 13 <robinsukh2023@gmail.com>
2025-08-24 23:12:39 +0530

    removed projects

--- Rubinder portfolio/src/App.tsx
+++ Rubinder portfolio/src/App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
-import Projects from './components/Projects';
+//import Projects from './components/Projects';
 import Achievements from './components/Achievements';
 import Education from './components/Education';
 import Footer from './components/Footer';
@@ -17,7 +17,6 @@ function App() {
       <About />
       <Skills />
       <Experience />
-      <Projects />
       <Achievements />
       <Education />
       <Footer />
